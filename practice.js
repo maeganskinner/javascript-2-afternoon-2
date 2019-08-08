b96d221351a3de3333ca77799a4af9c8418c24c3
maeganskinnerblog <maeganskinnerblog@gmail.com>
2019-08-08 16:16:59 -0500

    Commit try

--- practice.js
+++ practice.js
@@ -18,6 +18,9 @@ var arr = [10,20,30];
 */
 
 //Code Here
+function first (arr){
+  return arr [10]
+}
 
 
 
@@ -33,6 +36,9 @@ var arr = [40,50,60];
 */
 
 //Code Here
+function last (arr) {
+  return arr [40]
+}
 
 
 
@@ -48,6 +54,13 @@ var family = ['Tyler', 'Jordyn', 'Ryan', 'Chelsey', 'Ireland'];
 */
 
 //Code Here
+function looper(family){
+  for (var i = 0; i < family.length; i++){
+    alert(family[i])
+  }
+  console.log(family)
+}
+looper(family)
 
 
 
@@ -63,6 +76,14 @@ var letters = ['A', 'B', 'C', 'D', 'E'];
 */
 
 //Code Here
+function reversedLooper(letters){
+  for (var i = letters.length - 1; i >= 0; i--){
+     alert(letters[i])
+     console.log(letters[i])
+  }
+   
+  } 
+  reversedLooper(letters)
 
 
 
@@ -78,7 +99,15 @@ var nums = [1,2,3,6,22,98,45,23,22,12];
 */
 
 //Code Here
-
+function eventFinder(nums){
+  for (var i = 0; i < nums.length; i++){
+    if (nums[i] % 2 !== 0){
+    delete nums[i];
+    }
+  }
+  return nums
+}
+eventFinder(nums)
 
 
 
